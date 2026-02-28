46d9f058eb0dec55c04aec57b3e58204044a8602
Diego <diegoperez@Diegos-MacBook-Air.local>
2026-02-28 22:40:07 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'basketball-rotation-v75';
+const CACHE_NAME = 'basketball-rotation-v76';
 const urlsToCache = [
   '/playstats-basketball/',
   '/playstats-basketball/index.html'
